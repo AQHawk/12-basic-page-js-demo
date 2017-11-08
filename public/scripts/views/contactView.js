bd1c2b604ec1bb6a2583fa1c383b68068414f342
Sam Hamm <sam@codefellows.com>
2017-11-07 16:43:07 -0800

    ready

---
+++ public/scripts/views/contactView.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// This is where we define the view for the "Contact" page
+
+(function(module) {
+  const contactView = {};
+  console.log('YO CONTACT VIEW from outside the route handler');
+
+  contactView.init = function() {
+    console.log('INSIDE CONTACTVIEW INIT');
+    $('#contacts').empty();
+    $('body').css('background-color', '#ddd');
+    $('#content').text('This is the dadgum Contacts page!!!').css('color', 'purple');
+    $.getJSON('./scripts/models/contact.json')
+      .then(function(data) {
+        console.log(data);
+        data.forEach(function(contact) {
+          var liEl = document.createElement('li');
+          liEl.innerHTML = `${contact.name} has the ID number of ${contact.id} and a favorite number of ${contact['favorite number']}.`;
+          $(liEl).attr('data-ninja', contact.ninja);
+          $('#contacts').append(liEl);
+        });
+      }, function(err) {
+        console.error(err);
+      });
+  }
+
+  module.contactView = contactView;
+})(window);
